56485b947fa84254c21e051d7e7c140ca61b5010
Xueyu Wang <xueyuw@gatech.edu>
2020-10-16 22:46:18 -0400

    show address (import failed)

--- Taidl-react-app/App.js
+++ Taidl-react-app/App.js
@@ -1,7 +1,24 @@
-import * as React from "react";
+//import * as React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { View, Text } from "react-native";
+import useUserProvider from "./hooks/UserProvider";
+import JsonRpcProvider from "@ethersproject/providers";
+import useUserAddress from "eth-hooks";
+
+const DEBUG = true
+
+// 🏠 Your local provider is usually pointed at your local blockchain
+const localProviderUrl = "http://localhost:8545"; // for xdai: https://dai.poa.network
+// as you deploy to other networks you can set REACT_APP_PROVIDER=https://dai.poa.network in packages/react-app/.env
+const localProviderUrlFromEnv = process.env.REACT_APP_PROVIDER ? process.env.REACT_APP_PROVIDER : localProviderUrl;
+if(DEBUG) console.log("🏠 Connecting to provider:", localProviderUrlFromEnv);
+const localProvider = new JsonRpcProvider(localProviderUrlFromEnv);
 
 export default function App() {
+  const [injectedProvider, setInjectedProvider] = useState();
+  const userProvider = useUserProvider(injectedProvider, localProvider);
+  const address = useUserAddress(userProvider);
+
   return (
     <View
       style={{
@@ -11,6 +28,7 @@ export default function App() {
       }}
     >
       <Text>Universal React with Expo</Text>
+      <Text>'${address}'</Text>
     </View>
   );
 }
